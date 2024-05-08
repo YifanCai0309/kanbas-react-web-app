5280dc07af3c870742ad1c9573cd0fe22ffffada
yifancai <eve.yifan.cai@gmail.com>
2024-05-08 22:59:16 +0800

    added link to github

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import './App.css';
-import Labs from './Labs';
+import React from "react";
+import "./App.css";
+import Labs from "./Labs";
 
 function App() {
   return (
-   <div>
-    <h1 >Welcome to Web Dev</h1>
-    <Labs/>
-   </div>
+    <div>
+      <h1>Yifan Cai</h1>
+      <Labs />
+      <a href="https://github.com/YifanCai0309/kanbas-react-web-app/tree/a1">
+        GitHub
+      </a>
+    </div>
   );
 }
 
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,9 +4,6 @@ import Lab3 from "./Lab3";
 export default function Labs(){
     return(
         <div>
-            <h1>
-                Labs
-            </h1>
             <Lab1/>
             <Lab2/>
             <Lab3/>
